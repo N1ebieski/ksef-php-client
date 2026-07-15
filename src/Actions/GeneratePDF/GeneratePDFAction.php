c4646b7688eafa4f94e44050277a41d02eee3846
N1ebieski <kontakt@intelekt.net.pl>
2026-07-15 11:45:30 +0200

    Fixes #221 Support for ksef-pdf-generator v1.1.25

--- src/Actions/GeneratePDF/GeneratePDFAction.php
+++ src/Actions/GeneratePDF/GeneratePDFAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace N1ebieski\KSEFClient\Actions\GeneratePDF;
 
+use DateTimeInterface;
 use N1ebieski\KSEFClient\Actions\AbstractAction;
 use N1ebieski\KSEFClient\DTOs\QRCodes;
 use N1ebieski\KSEFClient\ValueObjects\KsefFeInvoiceConverterPath;
@@ -18,7 +19,9 @@ final class GeneratePDFAction extends AbstractAction
         public readonly ?string $upoDocument = null,
         public readonly ?string $confirmationDocument = null,
         public readonly ?KsefNumber $ksefNumber = null,
+        public readonly ?DateTimeInterface $acquisitionDate = null,
         public readonly ?QRCodes $qrCodes = null,
+        public readonly ?string $watermark = null,
     ) {
     }
 }
