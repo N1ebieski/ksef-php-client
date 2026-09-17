3b14a0077ad61af0c97911a6d37d1ea279120b61
webard <b.gajda@webard.me>
2026-09-17 11:03:01 +0200

    rector

--- rector.php
+++ rector.php
@@ -12,26 +12,15 @@ return \Rector\Config\RectorConfig::configure()
         \N1ebieski\KSEFClient\Overrides\Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector::class,
     ])
     ->withSkip([
-        \Rector\Carbon\Rector\MethodCall\DateTimeMethodCallToCarbonRector::class,
         \Rector\DeadCode\Rector\Node\RemoveNonExistingVarAnnotationRector::class,
-        \Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class,
         \Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector::class,
-        \Rector\Carbon\Rector\New_\DateTimeInstanceToCarbonRector::class,
-        \Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector::class => [
-            __DIR__ . '/src/Actions/ConvertEcdsaDerToRaw/ConvertEcdsaDerToRawHandler.php'
-        ],
-        \Rector\CodingStyle\Rector\FuncCall\FunctionFirstClassCallableRector::class => [
-            __DIR__ . '/src/Validator/Rules/Number/NipRule.php'
-        ],
         \Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector::class => [
             __DIR__ . '/src/Testing/Fixtures/Requests/AbstractResponseFixture.php',
             __DIR__ . '/src/Testing/Fixtures/DTOs/Requests/Sessions/AbstractFakturaFixture.php'
         ],
         \Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector::class,
         \Rector\DeadCode\Rector\Property\RemoveUselessVarTagRector::class => [
-            __DIR__ . '/src/Factories/EncryptedTokenFactory.php',
-            __DIR__ . '/src/Actions/SignDocument/SignDocumentHandler.php',
-            __DIR__ . '/src/Factories/EncryptedKeyFactory.php'
+            __DIR__ . '/src/Actions/SignDocument/SignDocumentHandler.php'
         ]
     ])
     ->withComposerBased(phpunit: true)
@@ -44,8 +33,6 @@ return \Rector\Config\RectorConfig::configure()
         privatization: true,
         instanceOf: true,
         earlyReturn: true,
-        carbon: true,
         phpunitCodeQuality: true
     )
-    ->withDowngradeSets(php81: true)
-    ->withPhpSets(php81: true);
+    ->withPhpSets(php84: true);
